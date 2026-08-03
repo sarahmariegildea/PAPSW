400637794f5fe2097c586b6d5e7473872aa43b51
sarahmariegildea <gildea@hcp.med.harvard.edu>
2026-08-02 20:12:18 -0400

    rpackeges

--- packages.R
+++ packages.R
@@ -4,10 +4,9 @@ install.packages("devtools")
 # install our ID529tutorials package
 devtools::install_github("ID529/ID529tutorials")
 
-library(ID529tutorials)
-available_tutorials('ID529tutorials')
-run_tutorial('dataframes_and_matrices', 'ID529tutorials')
-
-install.packages("library")
-install.packages("renv")
-installed.packages()
+#dependencies
+#read data
+#clean data
+#merge data
+#model
+#report on model
